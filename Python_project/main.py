284bb57033f9ec943add39a14b9a35169a26cb60
shivani-shvn <thyshivani@gmail.com>
2024-12-31 13:23:50 +0530

    Testing purpose

---
+++ Python_project/main.py
@@ -0,0 +1,78 @@
+#Python Calculator
+
+# operator = input("Enter an operator(+ - * /): ")
+# num1 = float(input("Enter the 1st number: "))
+# num2 = float(input("Enter the 2nd number: "))
+
+# if operator == "+":
+#     result = num1 + num2
+#     print(round(result, 3))
+# elif operator == "-":
+#     result = num1 - num2
+#     print(round(result, 3))
+# elif operator == "*":
+#     result = num1 * num2
+#     print(round(result, 3))
+# elif operator == "/":
+#     result = num1 / num2
+#     print(round(result, 3))
+# else:
+#     print(f"{operator} is not a valid operator")
+
+
+#Python weight converter
+
+# weight = float(input("Enter your weight: "))
+# unit = input("Kilograms or Pounds ('K or L): ")
+
+# if unit == "K":
+#     weight = weight * 2.205
+#     unit = "Lbs"
+#     print(f"Your weight is {round(weight, 2)} {unit}")
+# elif unit == "L":
+#     weight = weight / 2.205
+#     unit = "Kgs"
+#     print(f"Your weight is {round(weight, 2)} {unit}")
+# else :
+#     print(f"{unit} is not valid.")
+
+#Temperature conversion program
+
+# unit = input("Is this temperature in C/F : ")
+# temp = float(input("Enter the temperature: "))
+
+# if unit == "C":
+#     temp = round((temp * 9/5) + 32, 1)
+#     print(f"The temperature in Ferenhite is {temp}°F")
+# elif unit == "F":
+#     temp = round((temp - 32)* 5/9, 1)
+#     print(f"The temperature in Celsius is {temp}°C")
+# else:
+#     print(f"{unit} is invalid")
+
+#Logical operators : or, and ,not
+# temp = float(input("Enter the temperature: "))
+
+# is_raining = False
+
+# if temp > 35 or temp < 0 or is_raining :
+#     print(f"Don't go outside")
+
+# else:
+#     print(f"Awesome weather! GO outside")
+
+# temp = float(input("Enter the temperature: "))
+# is_sunny = True
+
+# if temp>25 and is_sunny:
+#     print(f"Outside is HOT!!🥵")
+#     print(f"It is Sunny☀️")
+# else:
+#     print(f"Outside is cold❄️")
+
+# num = 2
+
+# result = "EVEN" if num % 2 ==0 else 'ODD'
+# print(result)
+
+
